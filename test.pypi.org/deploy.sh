144d7be6d457923112e64327cb3d2bedd61d8c4e
Chentao Yang <comery@foxmail.com>
2018-11-22 14:45:20 +0800

    update

--- test.pypi.org/deploy.sh
+++
@@ -1,7 +0,0 @@
-# generate your package
-python setup.py sdist
-# upload your package to test pypi repository
-twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-
-# intall your package form test.pypi
-# pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple  hifise
